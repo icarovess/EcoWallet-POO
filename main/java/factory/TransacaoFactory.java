b531b0ddfde30d2f901538177773688e70132ef8
icarovess <icarocontato123@hotmail.com>
2026-05-19 16:54:08 -0300

    Adição de classes: TransacaoFactory e GerenciadorFinancas. Agora é possível identificar o tipo de transação (receita ou despesa), ainda é necessário implementar o código da classe GerenciadoFinancas com ArrayList para armazenar as transações e métodos para calcular o saldo, total de receitas e despesas, etc.

---
+++ main/java/factory/TransacaoFactory.java
@@ -0,0 +1,18 @@
+package main.java.factory;
+
+import java.time.LocalDate;
+
+import main.java.model;
+
+public class TransacaoFactory {
+
+    public static Transacao criarTransacao(String tipo, String descricao, Double valor, LocalDate data, Categoria categoria){
+        if(tipo.equalsIgnoreCase("receita")){
+            return new Receita(descricao, valor, data, categoria);
+        }
+        else if(tipo.equalsIgnoreCase("despesa")){
+            return new Despesa(descricao, valor, data, categoria);
+        }
+        return null;
+        }
+    }
