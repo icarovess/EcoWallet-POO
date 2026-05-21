097b46de07d1631a9330063f9029be448b2c0a60
Ícaro Pinheiro <icarocontato123@hotmail.com>
2026-05-21 19:07:19 -0300

    Implement finance manager with transaction handling

--- main/java/service/GerenciadorFinancas.java
+++ main/java/service/GerenciadorFinancas.java
@@ -1,2 +1,36 @@
 package main.java.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import main.java.model.*;
+
+public class GerenciadorFinancas {
+
+    private List<Transacao> transacoes;
+
+    public GerenciadorFinancas() {
+        transacoes = new ArrayList<>();
+    }
+
+    //adicionar transição na lista
+    public void adicionarTransicao(Transacao transacao) {
+        transacoes.add(transacao);
+    }
+
+
+    //Calcular o saldo.
+    public double calcularSaldo(){
+        double saldo = 0;
+
+        for (Transacao transacao : transacoes) {
+            saldo += transacao.getValor();
+        }
+
+        return saldo;
+    }
+
+    //Setter lista de transição
+    public List<Transacao> getTransacoes() {
+        return transacoes;
+    }  
+}
